fcd8b045fc5afe94a2d74b3501d655ca9932c2d2
trungnv <trungnv@ttc-solutions.com>
2020-02-07 15:11:47 +0700

    Initial Commit

---
+++ ClosureExp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClosureExp
+//
+//  Created by TrungNV on 2/7/20.
+//  Copyright © 2020 trungnv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
